18e19be1aacc04615ed859f52e95abc6270b0f8d
MerickBao <66370724+MerickBao@users.noreply.github.com>
2023-08-18 22:01:28 +0800

    1

---
+++ src/Atcoder/ABC315/C.java
@@ -0,0 +1,78 @@
+package Atcoder.ABC315;
+
+/**
+ * @author: merickbao
+ * @desc:
+ * @since: 2023/8/18 21:52
+ */
+
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class C {
+    public static Reader in;
+    public static PrintWriter out;
+    public static void main(String[] args) {
+        out = new PrintWriter(new BufferedOutputStream(System.out));
+        in = new Reader();
+        solve();
+        out.close();
+    }
+
+    static void solve(){
+        int t = in.nextInt();
+        while (t-- > 0) {
+
+        }
+    }
+
+    static class Reader {
+        private BufferedReader br;
+        private StringTokenizer st;
+
+        Reader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            try {
+                while (st == null || !st.hasMoreTokens()) {
+                    st = new StringTokenizer(br.readLine());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        int[] nextIntArray(int n) {
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++)
+                arr[i] = nextInt();
+            return arr;
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String s = "";
+            try {
+                s = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return s;
+        }
+    }
+}
